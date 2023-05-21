54123b3e226d5758a72730e0278a6f8686803408
Soti <58138432+SotiTheKing@users.noreply.github.com>
2023-05-21 22:49:03 +0300

    modified the form handling on the frontend/backend

--- form-handler.js
+++ form-handler.js
@@ -1,19 +1,18 @@
 const handleSubmit = (event) => {
-    event.preventDefault();
+  event.preventDefault();
+
+  const myForm = event.target;
+  const formData = new FormData(myForm);
   
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => alert("Thank you for your message!") && location.reload())
-      .catch((error) => alert('Something went wrong while trying to submit your message!') && console.log(error));
-  };
-  
-  document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
-  
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => alert("Thank you for your submission"))
+    .catch((error) => alert(error));
+};
+
+document
+  .querySelector("form")
+  .addEventListener("submit", handleSubmit);
