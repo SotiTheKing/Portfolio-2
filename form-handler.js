d0a76612a8dc9fd92141358fa1a51eb35d8c72d5
Soti <58138432+SotiTheKing@users.noreply.github.com>
2023-05-21 22:55:33 +0300

    replaced default success page for forms

--- form-handler.js
+++ form-handler.js
@@ -1,18 +1,19 @@
 const handleSubmit = (event) => {
-  event.preventDefault();
-
-  const myForm = event.target;
-  const formData = new FormData(myForm);
+    event.preventDefault();
   
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => alert("Thank you for your submission"))
-    .catch((error) => alert(error));
-};
-
-document
-  .querySelector("form")
-  .addEventListener("submit", handleSubmit);
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => alert("Thank you for your message!"))
+      .catch((error) => alert('Something went wrong while trying to submit your message!') && console.log(error));
+  };
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+  
